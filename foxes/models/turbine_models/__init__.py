24e2c41b674fb29423de17218e2c469f297bc1df
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2022-05-10 15:45:27 +0200

    new tmodel: set farm vars

--- foxes/models/turbine_models/__init__.py
+++ foxes/models/turbine_models/__init__.py
@@ -1,2 +1,3 @@
 from foxes.models.turbine_models.set_XYHD import SetXYHD
 from foxes.models.turbine_models.kTI import kTI
+from foxes.models.turbine_models.set_farm_vars import SetFarmVars
---
+++ foxes/models/turbine_models/set_farm_vars.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+from foxes.core import TurbineModel
+import foxes.constants as FC
+import foxes.variables as FV
+
+class SetFarmVars(TurbineModel):
+
+    def __init__(self):
+        super().__init__()
+
+        self.vars   = []
+        self._vdata = []
+    
+    def add_var(self, var, data):
+        self.vars.append(var)
+        self._vdata.append(data)
+    
+    def output_farm_vars(self, algo):
+        return self.vars
+    
+    def model_input_data(self, algo):
+        
+        idata = super().model_input_data(algo)
+
+        self._keys = {}
+        for i, v in enumerate(self.vars):
+
+            k = self.var(f"data_{v}")
+            self._keys[v] = k
+
+            idata["data_vars"][k] = ((FV.STATE, FV.TURBINE), self._vdata[i])
+        
+        del self._vdata
+
+        return idata
+    
+    def calculate(self, algo, mdata, fdata, st_sel):
+
+        n_states   = fdata.n_states
+        n_turbines = fdata.n_turbines
+        allt       = np.all(st_sel)       
+
+        for v in self.vars:
+
+            data  = mdata[self._keys[v]]
+            hsel  = ~np.isnan(data)
+            hallt = np.all(hsel)
+
+            if allt and hallt:
+                fdata[v] = data
+                
+            else:
+
+                if v not in fdata:
+                    fdata[v] = np.full((n_states, n_turbines), np.nan, dtype=FC.DTYPE)
+
+                tsel = st_sel & hsel
+                fdata[v][tsel] = data[tsel]
+
+        return {v: fdata[v] for v in self.vars}
