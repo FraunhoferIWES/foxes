4720fe8b775dd39b05df22961945c6cd63ff3944
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2022-05-18 07:43:53 +0200

    dev distsliced pwake

--- foxes/models/partial_wakes/partial_distsliced.py
+++ foxes/models/partial_wakes/partial_distsliced.py
@@ -3,7 +3,9 @@ import numpy as np
 from foxes.core import PartialWakesModel
 from foxes.models.wake_models.dist_sliced import DistSlicedWakeModel
 from foxes.models.rotor_models.grid import GridRotor
+from foxes.tools import wd2uv, uv2wd
 import foxes.variables as FV
+import foxes.constants as FC
 
 class PartialDistSlicedWake(PartialWakesModel):
 
@@ -36,7 +38,8 @@ class PartialDistSlicedWake(PartialWakesModel):
 
     def new_wake_deltas(self, algo, mdata, fdata):
 
-        n_points    = fdata.n_turbines
+        n_rpoints   = self.grotor.n_rotor_points()
+        n_points    = fdata.n_turbines * n_rpoints
         wake_deltas = {}
         for w in self.wake_models:
             w.init_wake_deltas(algo, mdata, fdata, n_points, wake_deltas)
@@ -58,10 +61,10 @@ class PartialDistSlicedWake(PartialWakesModel):
         n_states   = fdata.n_states
         n_turbines = fdata.n_turbines
         n_rpoints  = self.grotor.n_rotor_points()
-        points     = self.grotor.get_rotor_points(algo, mdata, fdata).reshape(n_states, n_turbines*n_rpoints, 3)
+        n_points   = n_turbines * n_rpoints
+        points     = self.grotor.get_rotor_points(algo, mdata, fdata).reshape(n_states, n_points, 3)
         wcoos      = self.wake_frame.get_wake_coos(algo, mdata, fdata, states_source_turbine, points)
         yz         = wcoos.reshape(n_states, n_turbines, n_rpoints, 3)[:, :, :, 1:3]
-        weights    = self.grotor.rotor_point_weights()
         del points, wcoos
 
         # evaluate wake models:
@@ -70,9 +73,15 @@ class PartialDistSlicedWake(PartialWakesModel):
             wdeltas, sp_sel = w.calc_wakes_spsel_x_yz(algo, mdata, fdata, 
                                                         states_source_turbine, x, yz)
             
+            wsps    = np.zeros((n_states, n_turbines, n_rpoints), dtype=bool)
+            wsps[:] = sp_sel[:, :, None]
+            wsps    = wsps.reshape(n_states, n_points)
+            
             for v, wdel in wdeltas.items():
 
-                d = np.einsum('ps,s->p', wdel, weights)
+                d = np.zeros((n_states, n_turbines, n_rpoints), dtype=FC.DTYPE)
+                d[sp_sel] = wdel
+                d = d.reshape(n_states, n_points)[wsps]
 
                 try:
                     superp = w.superp[v]
@@ -80,26 +89,48 @@ class PartialDistSlicedWake(PartialWakesModel):
                     raise KeyError(f"Model '{self.name}': Missing wake superposition entry for variable '{v}' in wake model '{w.name}', found {sorted(list(w.superp.keys()))}")
 
                 wake_deltas[v] = superp.calc_wakes_plus_wake(algo, mdata, fdata, states_source_turbine, 
-                                                            sp_sel, v, wake_deltas[v], d)
+                                                            wsps, v, wake_deltas[v], d)
                                                             
     def evaluate_results(self, algo, mdata, fdata, wake_deltas, states_turbine, update_amb_res=False):
         
-        weights = self.get_data(FV.RWEIGHTS, mdata)
-        amb_res = self.get_data(FV.AMB_RPOINT_RESULTS, mdata)
-        rpoints = self.get_data(FV.RPOINTS, mdata)
+        amb_res   = self.get_data(FV.AMB_RPOINT_RESULTS, mdata)
+        rpoints   = self.get_data(FV.RPOINTS, mdata)
+        rweights  = self.get_data(FV.RWEIGHTS, mdata)
+        wweights  = self.grotor.rotor_point_weights()
+        n_wpoints = self.grotor.n_rotor_points()
         n_states, n_turbines, n_rpoints, __ = rpoints.shape
+        st_sel    = (np.arange(n_states), states_turbine)
 
-        wres   = {}
-        st_sel = (np.arange(n_states), states_turbine)
+        uv = None
+        if FV.WS in amb_res and FV.WD in amb_res:
+            wd = amb_res[FV.WD].reshape(n_states, n_turbines, n_rpoints)[st_sel]
+            ws = amb_res[FV.WS].reshape(n_states, n_turbines, n_rpoints)[st_sel]
+            uv = wd2uv(wd, ws, axis=-1)
+            uv = np.einsum('spd,p->sd', uv, rweights)
+            del ws, wd
+        elif (FV.WS in amb_res and FV.WD not in amb_res) \
+            or (FV.WS not in amb_res and FV.WD in amb_res):
+            raise KeyError(f"Model '{self.name}': Missing one of the variables '{FV.WS}', '{FV.WD}' in ambient rotor results: {list(amb_res.keys())}")
+
+        wres = {}
         for v, ares in amb_res.items():
-            wres[v] = ares.reshape(n_states, n_turbines, n_rpoints)[st_sel]
-        del amb_res
+            if v == FV.WS:
+                wres[v] = np.linalg.norm(uv, axis=-1)
+            elif v == FV.WD:
+                wres[v] = uv2wd(uv, axis=-1)
+            else:
+                wres[v] = ares.reshape(n_states, n_turbines, n_rpoints)[st_sel]
+                wres[v] = np.einsum('sp,p->s', wres[v], rweights)
+            wres[v] = wres[v][:, None]
+        del amb_res, uv
 
         wdel = {}
         for v, d in wake_deltas.items():
-            wdel[v] = d.reshape(n_states, n_turbines, 1)[st_sel]
+            wdel[v] = d.reshape(n_states, n_turbines, n_wpoints)[st_sel]
         for w in self.wake_models:
             w.finalize_wake_deltas(algo, mdata, fdata, wres, wdel)
+        for v in wdel.keys():
+            wdel[v] = np.einsum('sp,p->s', wdel[v], wweights)[:, None]
 
         for v in wres.keys():
             if v in wake_deltas:
@@ -108,7 +139,7 @@ class PartialDistSlicedWake(PartialWakesModel):
                     mdata[FV.AMB_RPOINT_RESULTS][v][st_sel] = wres[v]
             wres[v] = wres[v][:, None]
         
-        self.rotor_model.eval_rpoint_results(algo, mdata, fdata, wres, weights, 
+        self.rotor_model.eval_rpoint_results(algo, mdata, fdata, wres, np.array([1.]), 
                                                 states_turbine=states_turbine)
 
     def finalize(self, algo, clear_mem=False):
