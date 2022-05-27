7f9bfe63368aa544169327e10b2e8f7b8bdffadf
Jonas Schmidt <jonasschmidt@posteo.de>
2022-05-27 08:07:39 +0200

    docstringing

--- foxes/core/turbine_model.py
+++ foxes/core/turbine_model.py
@@ -4,13 +4,72 @@ from abc import abstractmethod
 from foxes.core.farm_data_model import FarmDataModel
 
 class TurbineModel(FarmDataModel):
+    """
+    Abstract base class for turbine models.
+
+    Turbine models are FarmDataModels that run 
+    on a selection of turbines.
+
+    """
 
     def initialize(self, algo, st_sel):
+        """
+        Initializes the model.
+
+        Parameters
+        ----------
+        algo : foxes.core.Algorithm
+            The calculation algorithm
+        st_sel : numpy.ndarray of bool
+            The state-turbine selection,
+            shape: (n_states, n_turbines)
+
+        """
         super().initialize(algo)
 
     @abstractmethod
     def calculate(self, algo, mdata, fdata, st_sel):
+        """"
+        The main model calculation.
+
+        This function is executed on a single chunk of data,
+        all computations should be based on numpy arrays.
+
+        Parameters
+        ----------
+        algo : foxes.core.Algorithm
+            The calculation algorithm
+        mdata : foxes.core.Data
+            The model data
+        fdata : foxes.core.Data
+            The farm data
+        st_sel : numpy.ndarray of bool
+            The state-turbine selection,
+            shape: (n_states, n_turbines)
+
+        Returns
+        -------
+        results : dict
+            The resulting data, keys: output variable str.
+            Values: numpy.ndarray with shape (n_states, n_turbines)
+
+        """
         pass
 
     def finalize(self, algo, st_sel, clear_mem=False):
+        """
+        Finalizes the model.
+
+        Parameters
+        ----------
+        algo : foxes.core.Algorithm
+            The calculation algorithm
+        st_sel : numpy.ndarray of bool
+            The state-turbine selection,
+            shape: (n_states, n_turbines)
+        clear_mem : bool
+            Flag for deleting model data and
+            resetting initialization flag
+            
+        """
         super().finalize(algo, clear_mem)
--- foxes/core/turbine_order.py
+++ foxes/core/turbine_order.py
@@ -3,6 +3,13 @@ from foxes.core import FarmDataModel
 import foxes.variables as FV
 
 class TurbineOrder(FarmDataModel):
+    """
+    Abstract base class for turbine order models.
+
+    Turbine orders define the order of turbine wake
+    calculations for each state.
+    
+    """
 
     def output_farm_vars(self, algo):
         return [FV.ORDER]
