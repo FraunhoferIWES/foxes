3767d5c8b3d8091745985b1b9fdf14c15ff160d3
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2022-06-10 08:39:23 +0200

    dev docs

--- foxes/__init__.py
+++ foxes/__init__.py
@@ -1,3 +1,8 @@
+"""
+foxes
+
+Farm Optimization and eXtended yield Evaluation Software
+"""
 from foxes.core import WindFarm, Turbine
 from foxes.models import ModelBook
 
@@ -5,3 +10,7 @@ import foxes.algorithms
 import foxes.models
 import foxes.input
 import foxes.output
+
+# 'filled_by_setup.py'
+__version__ = '0.1.0'
+__release__ = '0.1.0'
