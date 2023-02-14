078fef86d70ba6b062212d59c6f1ac7c68dabdef
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2023-02-14 14:35:30 +0100

    adding utm conversion to geopandas utils

--- foxes/utils/geopandas_helpers.py
+++ foxes/utils/geopandas_helpers.py
@@ -4,24 +4,46 @@ import pandas as pd
 import argparse
 from pathlib import Path
 
+import foxes.constants as FC
+from .dict import Dict
+
 try:
     import geopandas as gpd
-    IMPORT_OK = True
+    IMPORT_GPD_OK = True
 except ImportError:
     gpd = None
-    IMPORT_OK = False
+    IMPORT_GPD_OK = False
 
-def check_import():
+try:
+    import utm
+    IMPORT_UTM_OK = True
+except ImportError:
+    utm = None
+    IMPORT_UTM_OK = False
+
+def check_import_gpd():
     """
     Checks if library import worked,
     raises error otherwise.
     """
-    if not IMPORT_OK:
+    if not IMPORT_GPD_OK:
         print("\n\nFailed to import geopandas. Please install, either via pip:\n")
         print("  pip install geopandas\n")
         print("or via conda:\n")
         print("  conda install -c conda-forge geopandas\n")
         raise ImportError("Failed to import geopandas")
+
+def check_import_utm():
+    """
+    Checks if library import worked,
+    raises error otherwise.
+    """
+    if not IMPORT_UTM_OK:
+        print("\n\nFailed to import utm. Please install, either via pip:\n")
+        print("  pip install utm\n")
+        print("or via conda:\n")
+        print("  conda install -c conda-forge utm\n")
+        raise ImportError("Failed to import utm")
 
 def read_shp(fname, **kwargs):
     """
@@ -40,7 +62,7 @@ def read_shp(fname, **kwargs):
         The data frame in WSG84
 
     """
-    check_import()
+    check_import_gpd()
     gpdf=gpd.read_file(fname, **kwargs)
     return gpdf.to_crs("EPSG:4326") # Convert to WGS84
 
@@ -74,7 +96,14 @@ def shp2csv(ifile, ofile, in_kwargs={}, out_kwargs={}, verbosity=1):
 
     return gpdf
 
-def read_shp_polygons(fname, names=None, name_col="Name", geom_col=None):
+def read_shp_polygons(
+        fname, 
+        names=None, 
+        name_col="Name", 
+        geom_col=None,
+        to_utm=True,
+        ret_utm_zone=False,
+    ):
     """
     Reads the polygon points from csv or shp file.
     
@@ -89,21 +118,28 @@ def read_shp_polygons(fname, names=None, name_col="Name", geom_col=None):
 
     Parameters
     ----------
-    fn: str
+    fname : str
         Path to the .csv or .shp file
-    names: list: of str, optinal
+    names : list: of str, optinal
         The names of the polygons to be extracted. All by
         default
-    name_col: int
+    name_col : int
         Column that contains the area names
     geom_col : str, optional
         The geometry column, default 'geom' or 'geometry'
+    to_utm : bool or str, optional
+        Convert to UTM coordinates. If str, then UTM zone
+        plus letter, e.g. "32U"
+    ret_utm_zone : bool
+        Return UTM zone plus letter as str
 
     Returns
     -------
-    dict:
+    point_dict : dict
         Dict with list of array of points. Key: area name,
         Value: list:np.ndarray, shape of latter: (n_points, 2)
+    utm_zone_str : str, optional
+        The utem zone plus letter as str, e.g. "32U"
 
     """
     if isinstance(fname, pd.DataFrame):
@@ -120,7 +156,7 @@ def read_shp_polygons(fname, names=None, name_col="Name", geom_col=None):
         geom_col = "geom" if "geom" in pdf.columns else "geometry"
     pdf[geom_col] = [str(g) for g in pdf[geom_col]]
 
-    out = {}
+    out = Dict()
     names = pdf.index.tolist() if names is None else names
     for name in names:
 
@@ -141,9 +177,24 @@ def read_shp_polygons(fname, names=None, name_col="Name", geom_col=None):
         if not multi:
             data = [data]
 
-        out[name] = [np.array(d) for d in  data]
+        out[name] = [np.array(d, dtype=FC.DTYPE) for d in  data]
     
-    return out
+    if isinstance(to_utm, str) or to_utm == True:
+        check_import_utm()
+        utmz = int(to_utm[:-1]) if isinstance(to_utm, str) else None
+        utml = to_utm[-1] if isinstance(to_utm, str) else None
+        for name in out.keys():
+            utm_polys = []
+            for poly in out[name]:
+                utm_polys.append(np.zeros_like(poly))
+                utm_polys[-1][:, 0], utm_polys[-1][:, 1], utmz, utml = utm.from_latlon(
+                    poly[:, 1], poly[:, 0], force_zone_number=utmz, force_zone_letter=utml)
+            out[name] = utm_polys
+
+    if ret_utm_zone:
+        return out, f"{utmz}{utml}"
+    else:
+        return out      
 
 if __name__ == "__main__":
 
