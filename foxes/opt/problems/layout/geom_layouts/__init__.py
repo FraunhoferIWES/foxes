b2bf30f52ed7e8430c162997ccc9af6fd770f95b
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2023-03-01 15:31:52 +0100

    dev gridded layout opt

--- foxes/opt/problems/layout/geom_layouts/__init__.py
+++ foxes/opt/problems/layout/geom_layouts/__init__.py
@@ -1,5 +1,6 @@
 from .geom_layout import GeomLayout
 from .geom_reggrid import GeomRegGrid
+from .geom_layout_gridded import GeomLayoutGridded
 from .geom_reggrids import GeomRegGrids
 from .objectives import OMaxN, OMinN, OFixN, MaxGridSpacing, MaxDensity, MeMiMaDist
 from .constraints import Valid, Boundary, MinDist, CMinN, CMaxN, CFixN, CMinDensity
