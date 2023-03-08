4200f533263e9ff346373913cbbb6a97d4d6ce5a
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2023-03-08 13:54:09 +0100

    new opt problem: FarmVarsPointProblem

--- foxes/opt/core/__init__.py
+++ foxes/opt/core/__init__.py
@@ -1,6 +1,6 @@
 from .farm_opt_problem import FarmOptProblem
 from .farm_point_opt_problem import FarmPointOptProblem
-from .farm_vars_problem import FarmVarsProblem
+from .farm_vars_problem import FarmVarsProblem, FarmVarsPointProblem
 from .farm_objective import FarmObjective
 from .farm_constraint import FarmConstraint
 from .pop_states import PopStates
