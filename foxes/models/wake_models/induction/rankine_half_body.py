9669d988c250e60fff0436952001fc4614fe8af2
MellBeli <belinda.mellish@iwes.fraunhofer.de>
2023-10-16 10:44:34 +0200

    added call to calc_wakes_plus_wake as not in parent class of RHB

--- foxes/models/wake_models/induction/rankine_half_body.py
+++ foxes/models/wake_models/induction/rankine_half_body.py
@@ -219,6 +219,25 @@ class RHB(WakeModel):
             wake_deltas["U"][sp_sel] = (vel_factor[:, None] * xyz[:, :2])[:,0]
             wake_deltas["V"][sp_sel] = (vel_factor[:, None] * xyz[:, :2])[:,1]
             
+            # BELINDA tried populating wake deltas WS and WD for calc_wakes_plus_wake below
+            # but this causes error in WD and doens't generate wake for first turbine anyway
+            #wake_deltas['WS'] = np.linalg.norm((wake_deltas['U'], wake_deltas['V']), axis=0)
+            #wake_deltas['WD'] = uv2wd(np.stack((wake_deltas['U'], wake_deltas['V']), axis=2))
+
+            ## BELINDA added call calc_wakes_plus_wake method as not included in parent class of RHB
+            for v, hdel in wake_deltas.items():
+                wake_deltas[v] = self.superp.calc_wakes_plus_wake(
+                    algo,
+                    mdata,
+                    fdata,
+                    pdata,
+                    states_source_turbine,
+                    sp_sel,
+                    v,
+                    wake_deltas[v],
+                    hdel[:, 0],
+                )
+                
         return wake_deltas 
 
     def finalize_wake_deltas(
