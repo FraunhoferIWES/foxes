1737ccdd5ca2f1b90c9e65365f9fe69784ad5842
MellBeli <belinda.mellish@iwes.fraunhofer.de>
2023-11-29 11:05:55 +0100

    adding plots with wd arrows to run.py

--- examples/foxes/induction_RHB/run.py
+++ examples/foxes/induction_RHB/run.py
@@ -165,3 +165,21 @@ if __name__ == "__main__":
     fig = next(g)
     plt.show()
 
+    # horizontal flow plot with wind direction
+    o = foxes.output.FlowPlots2D(algo, farm_results)
+    g = o.gen_states_fig_xy(args.var, resolution=2, quiver_pars=dict(angles="xy", scale_units="xy", scale=0.35),
+        quiver_n=4,xspace=120.0, yspace=120.0)
+    fig = next(g)
+    plt.savefig("RHB_xy.png")
+    plt.show()
+    plt.close(fig)
+
+    # vertical flow plot with wind direction
+    o = foxes.output.FlowPlots2D(algo, farm_results)
+    g = o.gen_states_fig_xz(args.var, resolution=2, quiver_pars=dict(angles="xy", scale_units="xy", scale=1.0),
+        quiver_n=4,xspace=100.0, zspace=120.0)
+    fig = next(g)
+    plt.savefig("RHB_xz.png")
+    plt.show()
+    plt.close(fig)
+
