8734d91b0d1f863b5a47b8f404fdacead43dc137
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2024-01-16 08:35:19 +0100

    RHB: removing cut by induction

--- foxes/models/wake_superpositions/__init__.py
+++ foxes/models/wake_superpositions/__init__.py
@@ -2,7 +2,7 @@
 Wake superposition models.
 """
 from .linear import LinearSuperposition
-from .quadratic import QuadraticSuperposition
+from .pow import PowSuperposition
 from .max import MaxSuperposition
 from .product import ProductSuperposition
 from .ti_superp import TISuperposition
