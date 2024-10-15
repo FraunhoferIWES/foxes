b52b1c81e85621f33a7686a1a328df6bb556774c
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2024-10-15 09:41:43 +0200

    dev mpi

--- foxes/engines/mpi.py
+++ foxes/engines/mpi.py
@@ -6,14 +6,17 @@ from .futures import ProcessEngine
 def load_mpi():
     """On-demand loading of the mpi4py package"""
     global MPIPoolExecutor
-    MPIPoolExecutor = import_module("mpi4py", hint="pip install mpi4py").MPIPoolExecutor
+    MPIPoolExecutor = import_module("mpi4py.futures", hint="pip install mpi4py").MPIPoolExecutor
     
 class MPIEngine(ProcessEngine):
     """
     The MPI engine for foxes calculations.
     
-    Run command, e.g. for n processors and a script run.py:
-    mpiexec -n 12 -m mpi4py.futures run.py
+    Examples
+    --------
+    Run command, e.g. for 12 processors and a script run.py:
+    
+    >>> mpiexec -n 12 -m mpi4py.futures run.py
 
     :group: engines
 
