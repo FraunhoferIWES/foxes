60d0ddb76672e628b2e99aeef8a83f868b05e5f0
Jonas Schulte <jonas.schulte@iwes.fraunhofer.de>
2024-11-06 14:58:30 +0100

    new states: SliceDataNC

--- foxes/input/states/__init__.py
+++ foxes/input/states/__init__.py
@@ -6,6 +6,7 @@ from .single import SingleStateStates
 from .scan_ws import ScanWS
 from .states_table import StatesTable, Timeseries, TabStates
 from .field_data_nc import FieldDataNC
+from .slice_data_nc import SliceDataNC
 from .multi_height import MultiHeightStates, MultiHeightTimeseries
 from .multi_height import MultiHeightNCStates, MultiHeightNCTimeseries
 from .one_point_flow import (
