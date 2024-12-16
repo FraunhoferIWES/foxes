ba5366c2cc91f9b786fbf4888b8fa36c7a47f717
Jonas Schulte <jonas.schulte@iwes.fraunhofer.de>
2024-12-16 09:31:46 +0100

    formatting

--- foxes/utils/wrg_utils.py
+++ foxes/utils/wrg_utils.py
@@ -16,7 +16,7 @@ class ReaderWRG:
     def __init__(self, fpath):
         """
         Constructor
-        
+
         Parameters
         ----------
         fpath: str
@@ -28,16 +28,23 @@ class ReaderWRG:
     def _prepare(self):
         # read the first two lines
         with open(self.fpath, "r") as fstream:
-            self._nx, self._ny, self._utmx0, self._utmy0, self._res = fstream.readline().split()
+            self._nx, self._ny, self._utmx0, self._utmy0, self._res = (
+                fstream.readline().split()
+            )
             second_line = fstream.readline().split()
             n_cols = len(second_line)
             self._n_sectors = int(second_line[8])
-            self._nx, self._ny, self._utmx0, self._utmy0, self._res \
-                = int(self._nx), int(self._ny), float(self._utmx0), float(self._utmy0), int(self._res)
+            self._nx, self._ny, self._utmx0, self._utmy0, self._res = (
+                int(self._nx),
+                int(self._ny),
+                float(self._utmx0),
+                float(self._utmy0),
+                int(self._res),
+            )
 
-        cols_sel = lambda name, secs : [f"{name}_{i}" for i in range(secs)]
+        cols_sel = lambda name, secs: [f"{name}_{i}" for i in range(secs)]
 
-        cols = [0] * (8+3*self._n_sectors)
+        cols = [0] * (8 + 3 * self._n_sectors)
         cols[0] = "utmx"
         cols[1] = "utmy"
         cols[2] = "z"
@@ -50,8 +57,9 @@ class ReaderWRG:
         cols[9::3] = cols_sel("As", self._n_sectors)
         cols[10::3] = cols_sel("Ks", self._n_sectors)
 
-        self._data = pd.read_csv(self.fpath, names=cols, skiprows=1,
-                                 sep='\s+', usecols=range(1, n_cols))
+        self._data = pd.read_csv(
+            self.fpath, names=cols, skiprows=1, sep="\s+", usecols=range(1, n_cols)
+        )
 
         self._data[cols_sel("fs", self._n_sectors)] /= 10
         self._data[cols_sel("As", self._n_sectors)] /= 10
@@ -61,11 +69,11 @@ class ReaderWRG:
     def data(self):
         """
         The WRG data
-        
+
         Returns
         -------
         df: pandas.DataFrame
             The WRG data
-        
+
         """
         return self._data
